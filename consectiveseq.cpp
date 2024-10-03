d1689f5465c0a98ff66852f6b4e0865789f04bd0
Mehboob <mehboobarshad300@gmail.com>
2024-10-03 09:55:36 +0500

    comit

---
+++ consectiveseq.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+  int sequence(vector<int> nums){
+    int n =  nums.size();
+    int seq = 1;
+    map<int,int> mp;
+    map<int,int>:: iterator it;
+    
+    for(int i = 0;i<n;i++){
+        mp[nums[i]] = nums[i];
+    }
+    int counter = 1;
+
+    for(auto it = mp.begin();it != mp.end();++it){
+        int num = it->first;
+        cout<<it->first<<endl;
+        if(mp.find(num+1) != mp.end()){
+            counter++;
+           
+        }else if ( counter > seq){
+            seq = counter;
+            counter = 0;
+        }
+    }
+
+    return seq;
+  }
+
+int main(){
+    vector<int> input = {3,8,5,7,6};
+    vector<int> inp = {100, 200, 1, 3, 2, 4,5,7,8};
+    int seq = sequence(inp);
+    cout<<seq;
+    return 0;
+}
