87eaf84ab7c191153f80e3ce8d9383e10f583389
Mehboob <mehboobarshad300@gmail.com>
2024-10-03 10:02:05 +0500

    comit

--- consectiveseq.cpp
+++ consectiveseq.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<map>
 using namespace std;
+
   int sequence(vector<int> nums){
     int n =  nums.size();
     int seq = 1;
@@ -15,22 +16,23 @@ using namespace std;
 
     for(auto it = mp.begin();it != mp.end();++it){
         int num = it->first;
-        cout<<it->first<<endl;
+        cout<<it->first<<"--->"<<it->second<<endl;
         if(mp.find(num+1) != mp.end()){
             counter++;
-           
-        }else if ( counter > seq){
-            seq = counter;
-            counter = 0;
+        }else{
+               if(counter > seq){
+                   seq = counter;
+               }
+               
+               counter = 1;
+           }
         }
-    }
-
-    return seq;
+        return seq;
   }
 
 int main(){
     vector<int> input = {3,8,5,7,6};
-    vector<int> inp = {100, 200, 1, 3, 2, 4,5,7,8};
+    vector<int> inp = {100, 200, 1, 3, 2, 4,5,7,8,9,10,11,12,13,14,15};
     int seq = sequence(inp);
     cout<<seq;
     return 0;
